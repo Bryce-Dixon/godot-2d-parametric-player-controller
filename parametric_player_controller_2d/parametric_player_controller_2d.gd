73fd6daa96f657f8a3ac28668ec587803c741364
Bryce Dixon <bryce@brycedixon.dev>
2025-04-02 04:39:09 -0700

    Fully documented ParametricPlayerController2D

--- parametric_player_controller_2d/parametric_player_controller_2d.gd
+++ parametric_player_controller_2d/parametric_player_controller_2d.gd
@@ -1,6 +1,10 @@
 @tool
+## A player controller intended for 2D platformers.[br]
+## Allows designers to set various player-facing values which are more intuitive and better control the player experience compared to physics-facing ones.
 class_name ParametricPlayerController2D extends CharacterBody2D
 
+## Allows easy access of the "current" [ParametricPlayerController2D] [Node][br]
+## Will automatically be set by [method _ready]
 static var current: ParametricPlayerController2D
 
 ## Emitted when the character begins accelerating horizontally
@@ -26,13 +30,25 @@ signal reached_terminal_velocity
 ## Emitted when a collision occurs
 signal collided(collision: KinematicCollision2D)
 
+## Cached accessor for the [CollisionShape2D] used
 var shape: CollisionShape2D:
   get:
     if not is_instance_valid(shape):
       shape = get_node_or_null(^"CollisionShape2D")
     return shape
 
-var facing_right := true
+## Determines if the character is currently facing right
+var facing_right := true:
+  set(value):
+    if facing_right == value:
+      return
+    facing_right = value
+    if facing_right:
+      faced_right.emit()
+    else:
+      faced_left.emit()
+## Determines if the character is currently facing left[br]
+## Automatically determined as the inverse of [member facing_right]
 var facing_left: bool:
   get:
     return not facing_right
@@ -40,12 +56,15 @@ var facing_left: bool:
     facing_right = not value
 
 @export_group("Collider", "collider_")
+## Radius of the [CapsuleShape2D] used by [member shape]
 @export var collider_radius := 10.0:
   set(value):
     shape.shape.radius = value
     shape.position.y = shape.shape.height * -0.5
   get:
     return shape.shape.radius
+## Height of the [CapsuleShape2D] used by [member shape][br]
+## Setting this will automatically reposition [member shape] so its bottom is aligned to this node's position.
 @export var collider_height := 30.0:
   set(value):
     shape.shape.height = value
@@ -55,6 +74,7 @@ var facing_left: bool:
 
 ## Replace this from code under different situations to enable different movement styles (eg: walking vs running vs crouched)
 @export var movement_data := ParametricPlayerController2DMovementData.new()
+## Value that [member CharacterBody2D.velocity][code].x[/code] is currently moving toward.
 var goal_horizontal_velocity: float
 ## Can be overridden to specify custom acceleration behavior (eg: dashing)
 func _get_horizontal_acceleration() -> float:
@@ -89,34 +109,47 @@ func is_decelerating_horizontally() -> bool:
   )
 
 @export_group("Inputs", "input_")
+## Input data for moving the character left.
 @export var input_left := ParametricPlayerController2DInputData.new(&"ui_left", 1)
+## Input data for moving the character right.
 @export var input_right := ParametricPlayerController2DInputData.new(&"ui_right", 1)
+## Input data for having the player jump.
 @export var input_jump := ParametricPlayerController2DInputData.new(&"ui_jump", 8)
-## Buffer of the character's grounded state to allow grounded-only actions (eg: jumping) to occur a short period after the player has left the ground
+## Buffer of the character's grounded state to allow grounded-only actions (eg: jumping) to occur a short period after the player has left the ground.
 @export var input_coyote_time := ParametricPlayerController2DBitBuffer.new()
-## Arbitrary list of input actions which will be kept up to date and accessible in custom scripts
+## Arbitrary list of input actions which will be kept up to date and accessible in custom scripts.
 @export var input_actions: Dictionary[StringName, ParametricPlayerController2DInputData]
 ## If set to [code]true[/code], all inputs will be ignored and will retain their current buffer states.[br]
 ## Should usually be the same as [member pause_physics][br]
-## If modified, it usually makes sense to call [method clear_input_buffers]
+## If modified, it usually makes sense to call [method clear_input_buffers].
 var pause_inputs := false
 ## If set to [code]true[/code], the character will not move.[br]
-## Should usually be the same as [member pause_inputs]
+## Should usually be the same as [member pause_inputs].
 var pause_physics := false
 
+## Jump data to use when grounded.
 @export var jump_data := ParametricPlayerController2DJumpData.new()
-var jumping := false
-## Maximum falling speed
+## Jump data to use when in the air (eg: double jump).[br]
+## The first jump in the air after leaving the ground will be [code]aerial_jump_data[0][/code], the second would be [code]aerial_jump_data[1][/code], and so on.[br]
+## This means the player will have [code]N[/code] aerial jumps where [code]N[/code] is the size of [member aerial_jump_data].
+@export var aerial_jump_data: Array[ParametricPlayerController2DJumpData]
+## Internal use only; determines if the player is currently holding a jump.
+var _jumping := false
+## Current index into [member aerial_jump_data][br]
+## Reset to [code]0[/code] when becoming grounded. Incremented when [member input_jump] is pressed while in the air and [member _jumping] is [code]false[/code].
+var aerial_jump_index := 0
+## Maximum falling speed.[br]
+## Instead of modifying this from another script, [method _get_terminal_velocity] should be overridden instead so the "default" value can be retained.
 @export var terminal_velocity := 120.0
-## Can be overridden to specify custom falling behavior (eg: fast-falling while holding down)
+## Can be overridden to specify custom falling behavior (eg: fast-falling while holding down).
 func _get_terminal_velocity() -> float:
   return terminal_velocity
 
-## Can be overridden to specify custom falling behavior (eg: fast-falling while holding down)
+## Can be overridden to specify custom falling behavior (eg: fast-falling while holding down).
 func _get_gravity() -> float:
   if pause_physics:
     return 0.0
-  if not jumping and velocity.y < 0.0:
+  if not _jumping and velocity.y < 0.0:
     return jump_data.get_min_height_gravity()
   return jump_data.get_max_height_gravity()
 
@@ -128,8 +161,10 @@ func _ready() -> void:
   else:
     current = self
 
+## Internal use only; determines if the character was on the floor during the last [method _physics_process] call.
 var _was_grounded := false
-var _active_slide_collisions: PackedInt64Array = []
+## Internal use only; tracks the ongoing collisions from [method CharacterBody2D.move_and_slide] calls.
+var _active_slide_collisions: PackedInt64Array
 func _physics_process(delta: float) -> void:
   if Engine.is_editor_hint():
     return
@@ -150,10 +185,8 @@ func _physics_process(delta: float) -> void:
     )
     if velocity.x > 0.1 and facing_left:
       facing_right = true
-      faced_right.emit()
-    elif velocity.x < 0.1 and facing_right:
+    elif velocity.x < -0.1 and facing_right:
       facing_left = true
-      faced_left.emit()
     if was_moving_horizontally:
       if absf(velocity.x) < 0.1:
         stopped_moving_horizontally.emit()
@@ -165,19 +198,20 @@ func _physics_process(delta: float) -> void:
     if input_coyote_time.is_high():
       if not _was_grounded:
         _was_grounded = true
+        aerial_jump_index = 0
         landed.emit()
     else:
       if _was_grounded:
         _was_grounded = false
   if not pause_inputs and input_coyote_time.any_high():
     if input_jump.was_pressed():
-      jumping = true
+      _jumping = true
       velocity.y = jump_data.get_velocity()
       jumped.emit()
       input_coyote_time.fill_state(false)
-  if jumping:
+  if _jumping:
     if pause_inputs or not input_jump.is_down() or velocity.y >= 0.0:
-      jumping = false
+      _jumping = false
   var was_falling := velocity.y > 0.0
   var current_terminal_velocity := _get_terminal_velocity()
   var was_at_terminal_velocity := velocity.y >= current_terminal_velocity
